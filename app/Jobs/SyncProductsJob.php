6236e60f15462bab53c054b4cf431194a6d4e1b4
Ancient <bitbucket@pria.digital>
2022-02-22 13:39:25 +0300

    Sync WIP

---
+++ app/Jobs/SyncProductsJob.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Http\Controllers\Api\SynchronizerController;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+use Illuminate\Support\Facades\Log;
+
+
+class SyncProductsJob implements ShouldQueue
+{
+
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    //public $filePath;
+
+    /**
+     * Количество попыток выполнения задания.
+     *
+     * @var int
+     */
+    public $tries = 1;
+
+    public function __construct($filePath)
+    {
+        //$this->filePath = $filePath;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //Log::channel("jobs")->info("[ProcessSyncLogJob] START " . $this->id);
+        $result = SynchronizerController::get_sync_file_availability();
+        if(!$result){
+            error_telegram("get_sync_file_availability failed");
+        }
+
+        $reault = SynchronizerController::sy();
+
+        //Log::channel("jobs")->info("[ProcessSyncLogJob] FINISH " . $this->id);
+    }
+
+}
